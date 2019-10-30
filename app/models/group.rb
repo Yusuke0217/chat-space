8783e601ab3802c42cbc4e218caa99795b5e788f
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2019-10-29 23:45:27 -0700

    1-812

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,10 +4,10 @@ class Group < ApplicationRecord
   has_many :messages
   validates :name, presence: true, uniqueness: true
 
-  def show_end_message
-    if (end_message = messages.end).present?
-      if end_message.content?
-        end_message.content
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
       else
         '画像が投稿されています'
       end
