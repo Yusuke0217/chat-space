1e4364e6a25a09689acf2ee2d55893a5b83d9ac0
Yusuke0217 <56789748+Yusuke0217@users.noreply.github.com>
2019-11-04 20:21:31 -0800

    1-I-930

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,22 +3,36 @@ $(function() {
   var html = 
               `
               <div class="chat-group-user clearfix">
+                <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
                 <p class="chat-group-user__name">${user.name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
               </div>
-              `
+              `;
             $("#user-search-result").append(html);
-            console.log(html)
   }
 
-  function appendErrMsgToHTML(user) {
-    var html = `
-          <div class="chat-group-user clearfix">
-            <p class="chat-group-user__name">${user}</p>
-          </div>
-          `
-          $("#user-search-result").append(html);
-  }
+  // function appendErrMsgToHTML(user) {
+  //   var html = `
+  //         <div class="chat-group-user clearfix">
+  //           <p class="chat-group-user__name">${user}</p>
+  //         </div>
+  //         `;
+  //         $("#user-search-result").append(html);
+  // }
+
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class="ChatMember__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".ChatMembers").append(html);
+  } 
+
+  // ここより上はテンプレートリテラル
+
+
+
 // 検索入力のタグである #user-search-field を指定しjQueryオブジェクトを取得します。「テキストフィールドに文字が入力されるたびにイベントを発火させるメソッド」はkeyupとし、入力するためにキーが押された後、上がるタイミングで処理が実行されるようにします。
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
@@ -47,7 +61,16 @@ $(function() {
         }
       })
       .fail(function() {
-        console.log("失敗です");
+        alert("失敗です");
       });
   });
+
+//".chat-group-user__btn--add"がクリックされたらイベント発火！
+// let 変数名 イベント内容 dataの中身の値を取得
+  $(document).on('click', ".chat-group-user__btn--add", function() {
+    let uname = $(this).data("user-name");
+    let uid = $(this).data("user-id");
+    $(this).parent().remove();
+  });
+
 });
